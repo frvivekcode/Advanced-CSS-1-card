9cd145f158786544e01413dca2889c07f741335c
frvivekcode <vivekmandal8810@gmail.com>
2026-01-09 22:28:01 +0530

    addjavascript

--- script.js
+++ script.js
@@ -7,3 +7,20 @@ body.addEventListener("mousemove",function(dets){
  
 
 })
+let card=document.querySelector(".card")
+card.addEventListener("mouseenter",function(){
+  card.style.animationPlayState = "running";
+      
+  card.style.border= "5px rgba(255, 255, 255, 0) solid";
+  card.style.boxShadow="0 0 40px rgb(255, 0, 157)"
+  card.style.color="rgb(255, 0, 102)"
+
+})
+card.addEventListener("mouseleave",function(){
+    card.style.animationPlayState="paused";
+    card.style.boxShadow="0 0 0px rgb(255, 0, 132)"
+    card.style.border="none"
+    card.style.color="white"
+
+    
+})
